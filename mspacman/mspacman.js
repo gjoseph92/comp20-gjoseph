d8e1d08be5d5da0408e712736986f79ce2771e75
Gabe Joseph <gabriel.joseph@tufts.edu>
2013-02-13 21:29:34 -0500

    Added ghost

--- mspacman/mspacman.js
+++ mspacman/mspacman.js
@@ -6,7 +6,8 @@ function drawPacman() {
 		board.src = 'pacman10-hp-sprite.png';
 		
 		ctx.drawImage(board, 321, 2, 465, 136, 0, 0, 465, 136);	//background
-
+		ctx.drawImage(board, 121, 2, 18, 18, 270, 114, 18, 18);	//ms. pac-man
+		ctx.drawImage(board, 63, 83, 15, 15, 283, 55, 15, 15);	//ghost
 	}
 	else {
 		error = document.createElement('h2');
