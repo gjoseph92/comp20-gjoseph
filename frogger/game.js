45d841f1c1b8591835872dcdc153076c8b76013d
Gabe Joseph <gabriel.joseph@tufts.edu>
2013-02-15 05:43:06 -0500

    Added check for HTML5 canvas compatibility, drafted functions and call structure.

--- frogger/game.js
+++ frogger/game.js
@@ -1,1 +1,34 @@
-// Your work goes here...
+function start_game() {
+	canvas = document.getElementById('game');
+	if (canvas.getContext) {
+		ctx = canvas.getContext('2d');
+		sprites = new Image();
+		sprites.src = "assets/frogger_sprites.png";
+		sprite_width = sprites.width;	//???
+		
+		drawBackground();
+	}
+	else {
+		error = document.createElement('h2');
+		error.textContent = "Oops, your browser doesn't support Canvas in HTML5.";
+		
+		wrapper = canvas.parentNode;
+		wrapper.replaceChild(error, canvas);
+	}
+}
+
+function drawBackground() {
+	//black;
+	//level elements
+	//water;
+	drawRoadBG(274, 236);
+	drawLily(57);
+	
+	drawOverlays(); //Header and footer
+}
+
+function drawRoadBG(y, height) { }
+
+function drawLily(y) { }
+
+function drawOverlays() { }
